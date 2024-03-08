26060444902f26e0768d68b53efd33dbca194b40
Erik Schneider <GiimPy159@gmail.com>
2024-03-08 14:08:52 +0100

    minor changes

--- src/main/java/com/Spring/controller/RestController.java
+++ src/main/java/com/Spring/controller/RestController.java
@@ -3,13 +3,16 @@ package com.Spring.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
 
-    @GetMapping("/aoc/{id}")
-    public String helloWorld(@PathVariable(name = "id") int id)
+    @GetMapping("/aoc/{year}/{day}")
+    public String helloWorld
+            (@PathVariable(name = "year") int year,
+             @PathVariable(name = "day") int day)
     {
-        return "how to frontend here? " + id;
+        return "how to frontend here? " + year + day;
     }
 }
