dc185eadf21c959d42811c279dad7e3c507adac7
Erik Schneider <GiimPy159@gmail.com>
2024-03-30 16:19:32 +0100

    changes: - moved most AOCService.java code to AOCRepository.java - enabled POST and GET for exercise input - added Getters and setters to AOCEntity.java - fixed bug for too long input - added search by ID function to AOCRepository.java

--- src/main/java/com/Spring/model/entities/AOCEntity.java
+++ src/main/java/com/Spring/model/entities/AOCEntity.java
@@ -8,6 +8,40 @@ public class AOCEntity {
     @EmbeddedId
     private AOCKey key;
     private String name;
+    @Column(columnDefinition = "TEXT")
     private String input;
+
+
+    public AOCEntity(int year, int day) {
+        key = new AOCKey(year, day);
+    }
+
+    public AOCEntity() {
+
+    }
+
+    public AOCKey getKey() {
+        return key;
+    }
+
+    public void setKey(AOCKey key) {
+        this.key = key;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getInput() {
+        return input;
+    }
+
+    public void setInput(String input) {
+        this.input = input;
+    }
 }
 
--- src/main/java/com/Spring/model/repositories/AOCRepository.java
+++ src/main/java/com/Spring/model/repositories/AOCRepository.java
@@ -3,7 +3,13 @@ package com.Spring.model.repositories;
 import com.Spring.model.entities.AOCEntity;
 import com.Spring.model.entities.AOCKey;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface AOCRepository extends JpaRepository<AOCEntity, AOCKey> {
 
+    //https://docs.spring.io/spring-data/jpa/reference/repositories/query-methods-details.html
+    @Query("SELECT e FROM AOCEntity e WHERE e.key.year = ?1 AND e.key.day = ?2")
+    AOCEntity findByYearAndDay(int year, int day);
+
+    //boolean existsByYearAndDay(int year, int day);
 }
